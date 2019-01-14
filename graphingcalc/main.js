b8e119fbb2a7bc7e2ded0eefeda127a7c9d06583
Gregory <gregtj30@gmail.com>
2019-01-14 14:11:23 -0800

    upgraded grapher

--- graphingcalc/main.js
+++ graphingcalc/main.js
@@ -3,14 +3,18 @@
 let offsetX = 400;
 let offsetY = 400;
 let mapFactor = 1;
-var zoom, showSolutions, zoomReset, a, b, c, xydisplay;
+var zoom, showSolutions, zoomReset, a, b, c, xydisplay, showSliders;
 let atext, atext2, btext, btext2, ctext, ctext2;
 let mouseIsDown = false;
 
-let startX, startY;
-let netPanningX = 0, netPanningY = 0;
 
-let zoomLevel = 1; 
+let startX, startY;
+let netPanningX = 400, netPanningY = 400;
+
+let zoomLevel = 3; 
+let zoomLevels = [.2, .4, .5 , 1, 2, 2.5, 4];
+
+
 let doDrawGraph = false;
 
 function setup() {
@@ -19,9 +23,10 @@ function setup() {
     
     background(255);
     frameRate(60);
+    setupCheckbox();
     setupInputs1();
     setupInputs2();
-    
+    showSliders.changed(changedInputs);
 
 }
 
@@ -31,7 +36,7 @@ function draw() {
     if (zoom.value() == 0) {
         zoom.value(1);
     } 
-
+    
     
     background(250);
     
@@ -58,7 +63,7 @@ function draw() {
     drawGraphs();
     }
     coolMouse();
-    
+    console.log(zoomLevel);
     
     zoomP.html('Scale: ' + zoom.value());
     //zoomReset.mouseIsPressed(zoom.value(100));
@@ -75,21 +80,13 @@ function graphBttn() {
 }
 
 function zoomOut() {
-    if(zoomLevel <= .5) {
-        zoomLevel -= .1;
-
-    } else if (zoomLevel <= .01) {
-        zoomLevel = .4;
-    } else {
-        zoomLevel -= .5;
+    if (zoomLevel > 0){
+        zoomLevel -= 1;
     }
-    //console.log(zoomLevel);
 }
 function zoomIn() {
-    if (zoomLevel < .5) {
-        zoomLevel += .1;
-    } else {
-    zoomLevel += .5;
+    if (zoomLevel < 6){
+        zoomLevel += 1;
     }
     
 }
@@ -130,14 +127,43 @@ function mouseDragged() {
         }
     }
 }
+function setupCheckbox() {
+    showSliders = createCheckbox('Replace text fields with sliders', false);
+    showSliders.position(1180-200, 100-15);
+}
 
+function changedInputs() {
+    a.remove();
+    b.remove();
+    c.remove();
+    a2.remove();
+    b2.remove();
+    c2.remove();
+    showSolutions.remove();
+    showVertex.remove();
+    atext.remove();
+    btext.remove();
+    ctext.remove();
+    atext2.remove();
+    btext2.remove();
+    ctext2.remove();
+    xydisplay.remove();
+    graph2.remove(); 
+    zoom.remove();
+    zoomP.remove();
+    setupInputs1();
+    setupInputs2();
+}
  
+
 
 
 
 //Creates HTML Elements
 
 function setupInputs1() {
+    
+
     zoom = createSlider(0, 400, 100, 2);
     zoom.position(20,80);
     zoom.style('width', '200px');
@@ -146,7 +172,8 @@ function setupInputs1() {
     zoomP.position(25, 85);
     
     
-    //zoomReset.mousePressed(resetZoom());
+    //zoomReset.mousePressed(resetzoom());
+    
 
     showSolutions = createCheckbox('Show solutions', false);
     showSolutions.position(1180-200, 100);
@@ -160,20 +187,32 @@ function setupInputs1() {
     atext.position(1180-200, 385-off);
 
     btext = createP('b (slope if a=0):');
-    btext.position(1180-291, 365-off);
+    btext.position(1180-291, 365-off-5);
 
     ctext = createP('c:');
-    ctext.position(1180-200, 345-off);
+    ctext.position(1180-200, 345-off-10);
+    
+    if (showSliders.checked() == false) {
+        a = createInput('0', 'number');
+        a.position(1200-200, 400-off);
+        a.style('100px');
 
-    a = createInput('0', 'number');
-    a.position(1200-200, 400-off);
-    a.style('100px');
+        b = createInput('0', 'number');
+        b.position(1200-200, 380-off-5);
 
-    b = createInput('0', 'number');
-    b.position(1200-200, 380-off);
+        c = createInput('0', 'number');
+        c.position(1200-200, 360-off-10);
+    } else if (showSliders.checked() == true) {
+        a = createSlider(-50, 50, 0, .1);
+        a.position(1200-200, 400-off);
+        //a.style('100px');
 
-    c = createInput('0', 'number');
-    c.position(1200-200, 360-off);
+        b = createSlider(-50, 50, 0, .1);
+        b.position(1200-200, 380-off-5);
+
+        c = createSlider(-10, 10, 0, 1);
+        c.position(1200-200, 360-off-10);
+    } 
 
 
     //mode = createCheckbox('Quadratic', false);
@@ -194,27 +233,40 @@ function setupInputs2() {
         atext2.position(1180-200, off2+25);
     
         btext2 = createP('b (slope if a=0):');
-        btext2.position(1180-291, off2+5);
+        btext2.position(1180-291, off2+5-5);
     
         ctext2 = createP('c:');
-        ctext2.position(1180-200, off2-15);
+        ctext2.position(1180-200, off2-15-10);
+
+    if(showSliders.checked() == false) {
     
         a2 = createInput('0', 'number');
         a2.position(1200-200, off2+40);
         a2.style('100px');
     
         b2 = createInput('0', 'number');
-        b2.position(1200-200, off2+20);
+        b2.position(1200-200, off2+20-5);
     
         c2 = createInput('0', 'number');
-        c2.position(1200-200, off2);
+        c2.position(1200-200, off2-10);
+    } else if(showSliders.checked() == true) {
+        a2 = createSlider(-50, 50, 0, .1);
+        a2.position(1200-200, off2+40);
+        a2.style('100px');
+    
+        b2 = createSlider(-50, 50, 0, .1);
+        b2.position(1200-200, off2+20-5);
+    
+        c2 = createSlider(-10, 10, 0, 1);
+        c2.position(1200-200, off2-10);
+    }
     
    
 
 
 } 
 
-function resetZoom() {
+function resetzoom() {
     zoom.value(100);
 }
 
@@ -245,24 +297,29 @@ function mouseWheel(event) {
 
 function drawGrid() {
     fill(0);
-  
+    
+    //console.log(offsetX, offsetY);
     textSize(15);
-    for (var x=-width*10; x < width*10; x+=100*zoomLevel) {
+    for (var x=-width*10; x < width*10; x+=100*zoomLevels[zoomLevel]) {
         stroke('grey');
-        line(x, -height*10, x, height*10);
+        if (x >= -width-offsetX && x <= width-offsetX) {
+        line(x, -height-offsetY, x, height-offsetY);
         strokeWeight(.5);
-		text(roundNum((x/zoom.value())/zoomLevel, 2), x+1, 12);
+        text(roundNum((x/zoom.value())/zoomLevels[zoomLevel], 2), x+1, 12);
+        }
 	}
-	for (var y=-height*10; y < height*10; y+=100*zoomLevel) {
+	for (var y=-height*10; y < height*10; y+=100*zoomLevels[zoomLevel]) {
         stroke('grey');
-        line(-width*10, y, width*10, y);
+        if (y >= -height-offsetY && y <= height-offsetY) {
+        line(-width-offsetX, y, width-offsetX, y);
         strokeWeight(.5);
-		text(roundNum((-y/zoom.value())/zoomLevel, 2), 1, y+12);
+        text(roundNum((-y/zoom.value())/zoomLevels[zoomLevel], 2), 1, y+12);
+        }
     }
     stroke(0);
     strokeWeight(1);
-    line(0, height*10, 0, -height*10);
-    line(-width*10, 0, width*10, 0);
+    line(0, height-offsetY, 0, -height-offsetY);
+    line(-width-offsetX, 0, width-offsetX, 0);
     strokeWeight(6);
   	stroke('blue');
     point(0,0);
