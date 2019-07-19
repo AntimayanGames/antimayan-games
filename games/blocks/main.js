8cec134297bba7739d2aa674256b17231c457a8b
Gregory Jackson <gregoryjackson@Gregorys-Mac-mini.local>
2019-07-19 11:27:37 -0400

    updated ap test

--- games/blocks/main.js
+++ games/blocks/main.js
@@ -4,7 +4,7 @@ let width = 1000;
 let height = 800;
 let bx = width - 130;
 let by = 500;
-let bx2 = width/2; 
+let bx2 = width / 2;
 let by2;
 let dig = 1;
 let p;
@@ -27,7 +27,7 @@ let click = document.getElementById("click");
 function setup() {
     createCanvas(width, height);
     frameRate(100);
-    
+
 }
 
 function draw() {
@@ -41,23 +41,24 @@ function draw() {
     //fill(0);
     makeBlock(m1, 100);
     makeBlock2(m2, 50);
+    
     for (i = 0; i < rate; i++) {
         calc();
-        
+
     }
     if (bx > 950) {
         textSize(50);
-        text("Click to restart", width/2, height/2); 
+        text("Click to restart", width / 2, height / 2);
 
     }
-    
+
     textAlign(RIGHT);
     textSize(38);
     fill(255);
     textFont('Helvetica');
     strokeWeight(1);
-    text("# Collisions: " + collisions, width-10, 40);
-   
+    text("# Collisions: " + collisions, width - 10, 40);
+
 }
 
 
@@ -65,7 +66,7 @@ function makeBlock(m, size) {
     by = 598 - size;
     //p = m*v;
 
-    
+
     stroke(255);
     strokeWeight(1);
     textSize(32);
@@ -75,21 +76,21 @@ function makeBlock(m, size) {
     strokeWeight(1);
     fill(0, 100, 230);
     rect(bx, by, size, size);
-    
-    
+
+
 }
 
 function makeBlock2(m, size) {
-    p2 = m*v2;
+    p2 = m * v2;
     by2 = 598 - size;
 
-    
+
     stroke(255);
     strokeWeight(1);
     textSize(32);
     textAlign(CENTER);
     fill(255);
-    text(m + " kg", bx2 + (size)/2, by2 - 10);
+    text(m + " kg", bx2 + (size) / 2, by2 - 10);
     strokeWeight(1);
     fill(200);
     rect(bx2, by2, size, size);
@@ -100,43 +101,43 @@ function calc() {
     if (bx < 100) {
         bx = 100;
     }
-     if(bx2+50 > bx) {
-       click.currentTime = 0;
-       click.play();
+    if (bx2 + 50 > bx) {
+        click.currentTime = 0;
+        click.play();
 
-       collisions++;
-       
-       v = ((m1 - m2)/(m1 + m2))*u1 + ((2*m2)/(m1 + m2)*u2);
-       bx2 = bx - 50;
-       v2 = ((m2 - m1)/(m1 + m2))*u2 + ((2*m1)/(m1 + m2)*u1);
+        collisions++;
+
+        v = ((m1 - m2) / (m1 + m2)) * u1 + ((2 * m2) / (m1 + m2) * u2);
+        bx2 = bx - 50;
+        v2 = ((m2 - m1) / (m1 + m2)) * u2 + ((2 * m1) / (m1 + m2) * u1);
     }
     bx2 += v2;
     bx += v;
     u1 = v;
     u2 = v2;
-    if(bx2 < 50) {
+    if (bx2 < 50) {
         // click.pause();
         click.currentTime = 0;
-         bx2 = 50;
-         v2 = -v2; 
-         collisions++;
-         click.play();
-     }  
+        bx2 = 50;
+        v2 = -v2;
+        collisions++;
+        click.play();
+    }
 }
 
 function mousePressed() {
-    if(mouseX >= 200 && mouseX <= 800 && mouseY >= 100 && mouseY <= 700) {
-    bx = width-400;
-    bx2 = width/2;
-    m1 = parseFloat(Minput.value);
-    m2 = parseFloat(minput.value);
-    rate = 10000;
-    if (m1 > 10000000) {
-        rate = m1/1000;
-    }
-    v = -2/rate;
-    v2 = 0;
-    collisions = 0;
-    init = true;
+    if (mouseX >= 200 && mouseX <= 800 && mouseY >= 100 && mouseY <= 700) {
+        bx = width - 400;
+        bx2 = width / 2;
+        m1 = parseFloat(Minput.value);
+        m2 = parseFloat(minput.value);
+        rate = 1000;
+        if (m1 > 10000000) {
+            rate = m1 / 1000;
+        }
+        v = -2 / rate;
+        v2 = 0;
+        collisions = 0;
+        init = true;
     }
 }
